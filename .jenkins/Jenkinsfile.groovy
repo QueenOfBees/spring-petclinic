e4cd010e83c85ab1f2b061cbf96c06004dc35dbb
Tuco <tuco@fanruan.com>
2024-09-24 21:02:27 +0800

    l3

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -5,7 +5,10 @@ pipeline {
     stages {
         stage('Example') {
             steps {
-                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}," +
+                        "branch is ${env.BRANCH_NAME}, change title is ${env.CHANGE_TITLE}"
+                echo "all env:>>>>"
+                echo env
             }
         }
     }
